2b35943036c6ba22f0315c603fe5f16a22107d49
B D Prabhutosh <bdprabhutosh@gmail.com>
2025-03-15 23:00:29 +0530

    Transaction cards beautification

--- credit-management-app-ui/src/environments/environment.ts
+++ credit-management-app-ui/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    url: (window as any)["NG_APP_BASE_URL"] || 'https://calm-youth-production.up.railway.app'
+    url: (window as any)["NG_APP_BASE_URL"] || 'http://localhost:8080'
 };
