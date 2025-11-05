2ddc69335de9ddc687dc739bbfb9b06acc994119
BD4424 <bdprabhutosh@gmail.com>
2025-11-05 08:53:40 +0530

    Cludflare settings 1

--- credit-management-app-ui/src/environments/environment.prod.ts
+++ credit-management-app-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    url: (window as any)["NG_APP_BASE_URL"] || 'https://northeast-camps-luke-reviewed.trycloudflare.com'
+    url: (window as any)["NG_APP_BASE_URL"] || 'https://moss-sets-urban-instance.trycloudflare.com'
     // url: (window as any)["NG_APP_BASE_URL"] || 'https://credit-management-app.ap-south-1.elasticbeanstalk.com'
 };
